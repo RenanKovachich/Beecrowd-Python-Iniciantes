31647e12c3bff18333540c90e2dece0afaa8885f
RenanKovachich <77703043+RenanKovachich@users.noreply.github.com>
2023-01-01 23:47:15 -0300

    Add files via upload
    +4 exercícios resolvidos no Beecrowd em Python 3

---
+++ Iniciantes Python/1183.py
@@ -0,0 +1,31 @@
+matrix = []
+operation = str(input())
+
+for i in range(12):
+    row = []
+
+    for j in range(12):
+        number = float(input())
+        row.append(number)
+    matrix.append(row)
+
+if operation == 'S':
+    sum = 0
+    c = 1
+    for l in range(0,11):
+        for k in range(c,12):
+            sum += matrix[l][k]
+        c += 1
+    print('{:.1f}'.format(sum))
+
+elif operation == 'M':
+    sum = 0
+    c = 1
+    c2 = 0
+    for l in range(0,11):
+        for k in range(c,12):
+            sum += matrix[l][k]
+            c2 += 1
+        c += 1
+    average = sum/c2
+    print('{:.1f}'.format(average))
