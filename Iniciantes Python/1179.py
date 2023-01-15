fbe4f428cf3c2dec7badf83c9b9861092019a61b
RenanKovachich <77703043+RenanKovachich@users.noreply.github.com>
2023-01-15 14:27:30 -0300

    Update 1179.py

--- Iniciantes Python/1179.py
+++ Iniciantes Python/1179.py
@@ -1,14 +1,33 @@
-x = 0
 par = []
-impar = []
-for j in range(15):
-    number = int(input())
-    if number % 2 == 0:
-        par.append(number)
-        print('par[{}] = {}'.format(j, par[j]))
-    else:
-        impar.append(number)
-        print('impar[{}] = {}'.format(j, impar[j]))
+imp = []
+cont = 0
+p = 0
+i = 0
 
-    
+while cont < 15:
+    x = int(input())
+    if x % 2 == 0:
+        par.append(x)
+        p = p + 1
+    if x % 2 != 0:
+        imp.append(x)
+        i = i + 1
+    if p > 4:
+
+        for c in range(0, 5):
+            print('par[{}] = {}'.format(c, par[c]))
+        par = []
+        p = 0
+    if i > 4:
+        for c in range(0, 5):
+            print('impar[{}] = {}'.format(c, imp[c]))
+        imp = []
+        i = 0
+    cont = cont + 1
+if i > 0:
+    for j in range(i):
+        print('impar[{}] = {}'.format(j, imp[j]))
+if p > 0:
+    for h in range(p):
+        print('par[{}] = {}'.format(h, par[h]))
         
