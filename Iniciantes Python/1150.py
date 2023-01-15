35ea8c0fd7650bfcc07a0583a2dfacd914bf8c80
RenanKovachich <77703043+RenanKovachich@users.noreply.github.com>
2023-01-15 14:21:55 -0300

    Update 1150.py

--- Iniciantes Python/1150.py
+++ Iniciantes Python/1150.py
@@ -1,1 +1,17 @@
-lista = list(map(int, input().split()))
+value1 = int(input())
+value2 = int(input())
+count = 0
+j = 1
+while value2 <= value1:
+    value2 = int(input())
+
+count = value1
+j = 1
+aux = count
+times = 0
+while aux < value2:
+    count = count + j
+    aux = count + aux
+    times += 1
+
+print(times+1)
