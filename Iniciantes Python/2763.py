96a56314f00bc73fc131a252cb19d305b536c433
Renan Kovachich <77703043+RenanKovachich@users.noreply.github.com>
2023-02-07 15:46:57 -0300

    feat: quinto e sexto desafio de 07/02/2023

---
+++ Iniciantes Python/2763.py
@@ -0,0 +1,7 @@
+cpf = str(input()).strip().split('.')
+a = cpf[2]
+
+print(cpf[0])
+print(cpf[1])
+print(a[:3])
+print(a[-2:])
---
+++ Iniciantes Python/2764.py
@@ -0,0 +1,5 @@
+dia, mes, ano = input().split('/')
+
+print('{}/{}/{}'.format(mes, dia, ano))
+print('{}/{}/{}'.format(ano, mes, dia))
+print('{}-{}-{}'.format(dia, mes, ano))
