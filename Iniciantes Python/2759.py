9dc4332aea93a3ce19c81a3936107e6716528f7d
Renan Kovachich <77703043+RenanKovachich@users.noreply.github.com>
2023-02-07 15:45:21 -0300

    feat: primeiro desafio de 07/02/2023

---
+++ Iniciantes Python/2759.py
@@ -0,0 +1,7 @@
+a = input()
+b = input()
+c = input()
+
+print('A = {}, B = {}, C = {}'.format(a, b, c))
+print('A = {}, B = {}, C = {}'.format(b, c, a))
+print('A = {}, B = {}, C = {}'.format(c, a, b))
