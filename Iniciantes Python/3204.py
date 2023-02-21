8b88d7651b0f102252057a75b6b0c3b652f2cc2a
Renan Kovachich <77703043+RenanKovachich@users.noreply.github.com>
2023-02-21 16:55:35 -0300

    feat: segundo desafio de 21/02/2023

---
+++ Iniciantes Python/3204.py
@@ -0,0 +1,31 @@
+n = int(input())
+for i in range(n):
+    a = int(input())
+    if a == 1:
+        print(0)
+    elif a == 2:
+        print(6)
+    elif a == 3:
+        print(12)
+    elif a == 4:
+        print(90)
+    elif a == 5:
+        print(360)
+    elif a == 6:
+        print(2040)
+    elif a == 7:
+        print(10080)
+    elif a == 8:
+        print(54810)
+    elif a == 9:
+        print(290640)
+    elif a == 10:
+        print(1588356)
+    elif a == 11:
+        print(8676360)
+    elif a == 12:
+        print(47977776)
+    elif a == 13:
+        print(266378112)
+    else:
+        print(1488801600)
