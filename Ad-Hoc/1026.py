fb7192ad84a781fa1218ec3b95281120c08c8243
Renan Kovachich <77703043+RenanKovachich@users.noreply.github.com>
2023-07-24 23:57:42 -0300

    Creating Ad-Hoc

---
+++ Ad-Hoc/1026.py
@@ -0,0 +1,11 @@
+def Main(a, b):
+    print(a ^ b)
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            a, b = map(int, input().split())
+        except EOFError:
+            break
+        Main(a, b)
