3ae0526b2a3a026111012b1bc70283f7a566e2d8
Renan Kovachich <77703043+RenanKovachich@users.noreply.github.com>
2023-08-12 12:10:55 -0300

    adding 2715

--- Iniciantes Python/2715.py
+++ Iniciantes Python/2715.py
@@ -1,16 +1,22 @@
-i = 2
 while True:
     try:
         n = int(input())
-        x = [int(k) for k in input().split()]
-        sub = sum(x[i:]) - sum(x[:i])
-        for j in range(2, len(x)):
-            i += 1
-            dif = sum(x[i:]) - sum(x[:i])
-            if dif < 0:
-                break
-            sub = dif
-        print(sub)
     except EOFError:
         break
-        
+
+    nums = list(map(int, input().split()))
+
+    if n == 1:
+        print(nums[0])
+        continue
+
+    rangel = 0
+    gugu = sum(nums)
+    for num in nums:
+        prev = abs(gugu - rangel)
+        rangel += num
+        gugu -= num
+        curr = abs(gugu - rangel)
+        if prev < curr:
+            print(prev)
+            break
